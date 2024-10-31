194b557b7495f863fa6f3ce07ff59e3f21ea6947
Alexis <alexis.cattaruzza@student.vinci.be>
2024-10-31 14:26:01 +0100

    add: front addFriends and FriendList components

---
+++ src/Components/Friend/AddFriends.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import config from '../../config';
+
+const AddFriend = () => {
+    const [username, setUsername] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        if (username.length > 2) {
+            axios.get(`${config.BASE_URL}/friend/searchUsers?username=${username}`)
+                .then(response => setSuggestions(response.data))
+                .catch(error => console.error('Error fetching suggestions:', error));
+        } else {
+            setSuggestions([]);
+        }
+    }, [username]);
+
+    const addFriend = (friendUsername) => {
+        axios.post(`${config.BASE_URL}/friend/friends`, { username: friendUsername })
+            .then(response => console.log('Friend added:', response.data))
+            .catch(error => console.error('Error adding friend:', error));
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                placeholder="Search for friends by username"
+            />
+            <ul>
+                {suggestions.map((suggestion, index) => (
+                    <li key={index} onClick={() => addFriend(suggestion.username)}>
+                        {suggestion.username}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default AddFriend;
---
+++ src/Components/Friend/FriendList.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import config from '../../config';
+
+const FriendList = () => {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        // Fetch friends list from the backend
+        axios.get(`${config.BASE_URL}/friend/getFriends`)
+            .then(response => setFriends(response.data))
+            .catch(error => console.error('Error fetching friends list:', error));
+    }, []);
+
+    return (
+        <ul>
+            {friends.map(friend => (
+                <li key={friend.id}>{friend.username}</li>
+            ))}
+        </ul>
+    );
+};
+
+export default FriendList;
