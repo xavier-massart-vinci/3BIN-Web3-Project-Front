1b9c286f58cb8cdb0370bd9262b8a28071a1d91a
xavier-massart-vinci <xavier.massart@student.vinci.be>
2024-11-16 21:59:47 +0100

    chore: dockerfile fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
+  const env = dotenv.config().parsed;
 
   return {
-    base: '/',
+    base: isProduction ? '/' : '/',
     plugins: [react()],
     define: {
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(
-        isProduction ? 'http://BACK_END_IP:4000' : 'http://localhost:3000'
-      ),
+      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env?.VITE_API_BASE_URL || 'http://localhost:3000'),
     },
   };
 });
