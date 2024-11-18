41b64dc89b89b294d181b912e36bad77b3f99478
xavier-massart-vinci <xavier.massart@student.vinci.be>
2024-11-18 11:39:34 +0100

    chore: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
+  const isProduction = mode === "production";
   const env = dotenv.config().parsed;
 
   return {
-    base: isProduction ? '/' : '/',
+    base: isProduction ? "/" : "/",
     plugins: [react()],
     define: {
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env?.VITE_API_BASE_URL || 'http://localhost:3000'),
+      "import.meta.env.VITE_API_BASE_URL": JSON.stringify(
+        env?.VITE_API_BASE_URL || "http://localhost:3000"
+      ),
     },
   };
 });
